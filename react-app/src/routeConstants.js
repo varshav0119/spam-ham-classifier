75435e76424c045c3bfdb93297488ebd17d8a0e6
Varsha Venkatasubramanian <varsha@Varshas-MacBook-Pro.local>
2022-02-14 01:33:10 +0530

    uploading dataset

--- react-app/src/routeConstants.js
+++ react-app/src/routeConstants.js
@@ -1,5 +1,5 @@
 export const environment = 'local'
 export const BASE_URL = {
     local: 'http://localhost:5000',
-    heroku: 'https://aryabota.herokuapp.com'
+    heroku: 'https://spam-ham-classifier-flask-app.herokuapp.com/'
 }
